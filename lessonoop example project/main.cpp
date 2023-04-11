83c66508fcf996a79331960dd0922c2f3fbee238
mbortt <bortnmax@mail.ru>
2023-04-11 18:47:52 +0300

    добавили в класс консьруктор копирования

--- lessonoop example project/main.cpp
+++ lessonoop example project/main.cpp
@@ -24,6 +24,24 @@ public:
 		avg_mark = avg;
 	}
 
+
+	Student(string nm, string surnm) {
+		cout << "constructor with arguments" << endl;
+		name = nm;
+		surname = surnm;
+		age = 15;
+		avg_mark = 4;
+	}
+
+	//copy constructr
+	Student( const Student & st) {
+		cout << "copy constructur" << endl;
+		name = st.name;
+		surname = st.surname;
+		age = st.age;
+		avg_mark = st.avg_mark ;
+	}
+
 	~Student() {
 		cout << "destructor" << endl;
 	}
@@ -50,10 +68,18 @@ public:
 
 int main() {
 
-	Student st1;
+	/*Student st1;*/
 	Student st2("Ivan", "Ivanov", 14, 10);
-	cout << st1.convert() << endl;
+	/*Student st3("Dima", "Spot");*/
+	Student st4 = st2, st5=st2, st6=st2;
+
+
+	/*cout << st1.convert() << endl;*/
 	cout << st2.convert() << endl;
+	/*cout << st3.convert() << endl;*/
+	cout << st4.convert() << endl;
+
+
 	/*st1.name = "ivan";
 	st1.surname = "ivanov";
 	st1.age = 14;
